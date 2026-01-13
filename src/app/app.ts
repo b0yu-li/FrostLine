2ae4a2da7f4e74dc98b72974bef301d932cfe77b
Boyu Li <lby1251853265@icloud.com>
2026-01-13 14:10:08 +0800

    feat: takes in a video instead of a hard-coded video

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,32 @@
-import { Component, signal } from '@angular/core';
+import {Component, signal} from '@angular/core';
+import {DomSanitizer, SafeUrl} from '@angular/platform-browser';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [NgIf],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('FrostLine');
+
+  videoUrl: SafeUrl | null = null;
+
+  constructor(private sanitizer: DomSanitizer) {
+  }
+
+  onVideoSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+
+    if (input.files && input.files[0]) {
+      const file = input.files[0];
+
+      // 1. Create a temporary URL for the selected file
+      const objectUrl = URL.createObjectURL(file);
+
+      // 2. Bypass security trust for this specific URL
+      this.videoUrl = this.sanitizer.bypassSecurityTrustUrl(objectUrl);
+    }
+  }
 }
